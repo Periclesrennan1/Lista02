76a9190185c7d2508b252c3c77f1f954ab4dee86
Periclesrennan1 <125756144+Periclesrennan1@users.noreply.github.com>
2023-02-26 16:19:13 -0300

    Add files via upload

---
+++ Lista02/src/Main.java
@@ -0,0 +1,74 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+       /* String data = mesPorExtenso(8, 10, 2005);
+
+        System.out.print(data);
+    }
+
+
+    public static String mesPorExtenso(int dia, int mes, int ano){
+
+        String nomeMes = "";
+
+        if(mes == 1){
+            nomeMes = "Janeiro";
+        } else if (mes == 2){
+            nomeMes = "Fevereiro";
+        } else if (mes == 3) {
+            nomeMes = "Março";
+        } else if (mes == 4) {
+            nomeMes = "Abril";
+        } else if (mes == 5) {
+            nomeMes = "Maio";
+        } else if (mes == 6) {
+            nomeMes = "Junho";
+        } else if (mes == 7) {
+            nomeMes = "Julho";
+        } else if (mes == 8) {
+            nomeMes = "Agosto";
+        } else if (mes == 9) {
+            nomeMes = "Setembro";
+        } else if (mes == 10) {
+            nomeMes = "Outubro";
+        } else if (mes == 11) {
+            nomeMes = "Novembro";
+        } else if (mes == 12) {
+            nomeMes = "Dezembro";
+        }
+
+        return String.format( "%d de %s de %s", dia, nomeMes, ano);
+
+    }*/
+
+/*
+
+        int soma = calcularRetornando(1, 5, 10);
+
+        System.out.print(soma);
+
+    }
+
+    public static int calcularRetornando(int num1, int num2, int num3) {
+        return (num1 + num2 + num3);
+    }
+
+*/
+
+
+/*
+
+        double soma = somaImposto(40, 80);
+
+        System.out.print(soma);
+    }
+
+    public static double somaImposto(double taxaImposto, double custo){
+        double resultado = (taxaImposto / 100) * custo;
+        return (resultado + custo);
+
+*/
+    }
+
+}
